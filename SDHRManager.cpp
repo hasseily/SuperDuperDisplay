d05f419123783fddb463d9cb9bdf9a290f0db00c
Henri Asseily <henri@asseily.com>
2023-05-21 22:11:16 +0300

    Perspective aspect ratio

--- SDHRManager.cpp
+++ SDHRManager.cpp
@@ -395,8 +395,8 @@ void SDHRManager::Render()
 
 	// TEST
 	// Using a prespective so I can zoom back and forth easily
-	mat_proj = glm::perspective<float>(120, 1, 0, 256);
-
+	// perspective uses (fov, aspect, near, far)
+	mat_proj = glm::perspective<float>(120, (float)_SDHR_WIDTH/_SDHR_HEIGHT, 0, 256);
 	// Render the windows (i.e. the meshes with the windows stencils)
 	for each (auto& _w in this->windows) {
 		if (_w.enabled) {
