7329a2dac14740c99825afd5d7d2d8e61d1bdb43
Henri Asseily <henri@asseily.com>
2024-02-15 18:47:25 +0200

    Fixed Cycle counting. Correctly switches to PAL.

--- CycleCounter.cpp
+++ CycleCounter.cpp
@@ -2,38 +2,46 @@
 #include <mutex>
 #include <iostream>
 
+enum class VideoRegion
+{
+	Unknown = 0,
+	NTSC = 1,
+	PAL = 2
+};
+
+
 // below because "The declaration of a static data member in its class definition is not a definition"
 CycleCounter* CycleCounter::s_instance;
 
-static uint32_t m_cycle;		// Current cycle
+static uint32_t m_cycle = 0;				// Current cycle
+static uint32_t m_cycle_alignments = 0;		// Number of times we aligned the cycles to the VBL seen
 static bool bIsHBL = true;
 static bool bIsVBL = false;
+static VideoRegion m_region;	// We default to NTSC values unless we get a VBL in a wrong cycle
 std::mutex mtx_cycle;	// protect the cycle counter
 
-const uint32_t cycles_scanline = 40; // each scanline is 40 bytes, 1 cycle per byte
-const uint32_t cycles_hblank = 25;   // always 25 cycles
-const uint32_t cycles_total = 17030;
+static uint32_t dbg_last_vbl_cycle = 0;
 
-struct VideoTiming {
-	const uint32_t count_scanlines;
-	const uint32_t cycles_screen;
-	const uint32_t cycles_vblank;
+const uint32_t COUNT_SCANLINES = 192;		// EVEN IN SHR with 200 visible lines, the VBL triggers at line 192.
+const uint32_t CYCLES_SCANLINES = 40;		// each scanline is 40 bytes, 1 cycle per byte
+const uint32_t CYCLES_HBLANK = 25;			// always 25 cycles
+const uint32_t CYCLES_SCREEN = (CYCLES_SCANLINES + CYCLES_HBLANK) * COUNT_SCANLINES;
+const uint32_t CYCLES_TOTAL_NTSC = 17030;
+const uint32_t CYCLES_TOTAL_PAL = 20280;
 
-	VideoTiming(uint32_t countScanlines)
-		: count_scanlines(countScanlines),
-		cycles_screen(countScanlines* (cycles_scanline + cycles_hblank)),
-		cycles_vblank(262 * (cycles_scanline + cycles_hblank) - cycles_screen) {}
-};
-
-static VideoTiming timingApple2Modes(192); // number of visible scanlines
-static VideoTiming timingSHR(200);
+uint32_t cycles_vblank;
+uint32_t cycles_total;
 
 void CycleCounter::Initialize()
 {
 	// So we can render on startup, move the cycle counter to after HBlank
 	std::lock_guard<std::mutex> lock(mtx_cycle);	// unlocked when goes out of scope
-	m_cycle = cycles_hblank;
+	m_cycle = CYCLES_HBLANK;
 	bIsHBL = false;
+
+	m_region = VideoRegion::NTSC;
+	cycles_total = CYCLES_TOTAL_NTSC;
+	cycles_vblank = cycles_total - CYCLES_SCREEN;
 }
 
 void CycleCounter::IncrementCycles(int inc, bool isVBL)
@@ -41,18 +49,42 @@ void CycleCounter::IncrementCycles(int inc, bool isVBL)
 	std::lock_guard<std::mutex> lock(mtx_cycle);
 	m_cycle += inc;
 	m_cycle = (m_cycle % cycles_total);
-	auto t = (isSHR ? timingSHR : timingApple2Modes);
 	if (isVBL)
 	{
-		if (m_cycle < t.cycles_screen)
+		if (m_cycle < CYCLES_SCREEN)
 		{
 			// std::cout << "	VBL in cycle: " << m_cycle << std::endl;
-			m_vbl_start = m_cycle;
-			m_cycle = t.cycles_screen;
+			// determine the region
+			if ((m_cycle_alignments > 0) && (m_cycle < 7000))	// shouldn't happen, so it's the other region
+			{
+				if (m_region == VideoRegion::NTSC)
+				{
+					m_region = VideoRegion::PAL;
+					cycles_total = CYCLES_TOTAL_PAL;
+					cycles_vblank = cycles_total - CYCLES_SCREEN;
+					std::cout << "	VBL in cycle: " << m_cycle << ". Switched to PAL." << std::endl;
+				}
+				else {
+					m_region = VideoRegion::NTSC;
+					cycles_total = CYCLES_TOTAL_NTSC;
+					cycles_vblank = cycles_total - CYCLES_SCREEN;
+					std::cout << "	VBL in cycle: " << m_cycle << ". Switched to NTSC." << std::endl;
+				}
+				m_vbl_start = CYCLES_SCREEN;
+				m_cycle = CYCLES_SCREEN;
+				m_cycle_alignments = 0;
+			}
+			else {
+				m_vbl_start = m_cycle;
+				m_cycle = CYCLES_SCREEN;
+				m_cycle_alignments++;
+				std::cout << "VBL Alignment " << m_cycle_alignments 
+					<< ": " << m_vbl_start << " ---> " << m_cycle << std::endl;
+			}
 		}
 	}
-	bIsVBL = (m_cycle >= t.cycles_screen);
-	bIsHBL = (GetByteYPos() < cycles_hblank);
+	bIsVBL = (m_cycle >= CYCLES_SCREEN);
+	bIsHBL = (GetByteYPos() < CYCLES_HBLANK);
 }
 
 const bool CycleCounter::IsVBL()
@@ -72,11 +104,11 @@ const bool CycleCounter::IsInBlank()
 
 const uint32_t CycleCounter::GetScanline()
 {
-	return m_cycle / (cycles_scanline + cycles_hblank);
+	return m_cycle / (CYCLES_SCANLINES + CYCLES_HBLANK);
 }
 
 const uint32_t CycleCounter::GetByteYPos()
 {
-	return m_cycle % (cycles_scanline + cycles_hblank);
+	return m_cycle % (CYCLES_SCANLINES + CYCLES_HBLANK);
 }
 
