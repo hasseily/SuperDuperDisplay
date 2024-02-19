59b8abbf9d3094ffa86ddc7bf4008902f6d54319
Henri Asseily <henri@asseily.com>
2024-02-19 21:53:38 +0200

    In process

---
+++ A2WindowBeam.cpp
@@ -0,0 +1,144 @@
+#include "A2WindowBeam.h"
+#include "common.h"
+#include <SDL_timer.h>
+#include "A2VideoManager.h"
+#include "SDHRManager.h"
+
+void A2WindowBeam::Reset()
+{
+	SetEnabled(false);
+}
+
+A2WindowBeam::~A2WindowBeam()
+{
+	if (DBTEX != UINT_MAX)
+		glDeleteTextures(1, &DBTEX);
+
+	if (VAO != UINT_MAX)
+	{
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+	}
+}
+
+void A2WindowBeam::Define(A2VideoModeBeam_e _video_mode, Shader* _shaderProgram)
+{
+	this->Reset();
+	video_mode = _video_mode;
+	shaderProgram = _shaderProgram;
+
+	switch (video_mode) {
+	case A2VIDEOBEAM_LEGACY:
+		screen_count = uXY({ 560u , 384u });
+		break;
+	case A2VIDEOBEAM_SHR:
+		screen_count = uXY({ 640u , 400u });
+		break;
+	default:
+		break;
+
+	}
+	vertices[0].PixelPos = glm::vec2(0				, screen_count.y);	// top left
+	vertices[1].PixelPos = glm::vec2(screen_count.x	, 0				);	// bottom right
+	vertices[2].PixelPos = glm::vec2(screen_count.x	, screen_count.y);	// top right
+	vertices[3].PixelPos = glm::vec2(0				, screen_count.y);	// top left
+	vertices[4].PixelPos = glm::vec2(0				, 0				);	// bottom left
+	vertices[5].PixelPos = glm::vec2(screen_count.x	, 0				);	// bottom right
+
+	bNeedsGPUVertexUpdate = true;
+
+}
+
+void A2WindowBeam::Render()
+{
+	if (!IsEnabled())
+		return;
+	if (shaderProgram == nullptr)
+		return;
+	if (vertices.size() == 0)
+		return;
+
+	GLenum glerr;
+	if (VRAMTEX == UINT_MAX)
+		glGenTextures(1, &VRAMTEX);
+
+	if (VAO == UINT_MAX)
+	{
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+	}
+
+	glBindVertexArray(VAO);
+
+	if (bNeedsGPUVertexUpdate)
+	{
+		// load data into vertex buffers
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(A2BeamVertex), &vertices[0], GL_STATIC_DRAW);
+
+		// set the vertex attribute pointers
+		// vertex relative Positions: position 0, size 2
+		// (vec4 values x and y)
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(A2BeamVertex), (void*)0);
+		// vertex pixel Positions: position 1, size 2
+		// (vec4 values z and w)
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(A2BeamVertex), (void*)offsetof(A2BeamVertex, PixelPos));
+	}
+
+	// TODO: Point to the VRAM
+	// TODO: at each cycle update the VRAM
+	uint8_t* data = ...;
+
+	// Associate the texture VRAMTEX in GL_TEXTURE0+_SDHR_TBO_TEXUNIT with the buffer
+	// This is the apple 2's memory which is mapped to a "texture"
+	// Always update that buffer in the GPU
+	uint32_t _h = 200;	// max 200 lines
+	glActiveTexture(GL_TEXTURE0 + _SDHR_TBO_TEXUNIT);
+	glBindTexture(GL_TEXTURE_2D, VRAMTEX);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8UI,	// Unsigned integer, not normalized
+		40,
+		200,
+		0, GL_RGB_INTEGER, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	// reset the binding
+	glBindVertexArray(0);
+
+	if ((glerr = glGetError()) != GL_NO_ERROR) {
+		std::cerr << "A2WindowBeam::Update error: " << glerr << std::endl;
+	}
+
+	bNeedsGPUVertexUpdate = false;
+
+	GLenum glerr;
+	shaderProgram->use();
+	if ((glerr = glGetError()) != GL_NO_ERROR) {
+		std::cerr << "OpenGL A2Video glUseProgram error: " << glerr << std::endl;
+		return;
+	}
+
+	glBindVertexArray(VAO);
+
+	shaderProgram->setInt("ticks", SDL_GetTicks());
+
+	// point the uniform at the VRAM texture
+	glActiveTexture(GL_TEXTURE0 + _SDHR_TBO_TEXUNIT);
+	glBindTexture(GL_TEXTURE_2D, VRAMTEX);
+	shaderProgram->setInt("VRAMTEX", _SDHR_TBO_TEXUNIT);
+	// back to the output buffer to draw our scene
+	glActiveTexture(GL_TEXTURE0);
+	glDrawArrays(GL_TRIANGLES, 0, (GLsizei)this->vertices.size());
+	glBindVertexArray(0);
+	if ((glerr = glGetError()) != GL_NO_ERROR) {
+		std::cerr << "A2WindowBeam render error: " << glerr << std::endl;
+	}
+}
+
+
---
+++ A2WindowBeam.h
@@ -0,0 +1,75 @@
+#pragma once
+#ifndef A2WINDOWBEAM_H
+#define A2WINDOWBEAM_H
+
+#include <vector>
+#include "shader.h"
+
+#define _A2_TEXT80_CHAR_WIDTH 7
+#define _A2_TEXT80_CHAR_HEIGHT 16
+#define _A2_TEXT40_CHAR_WIDTH _A2_TEXT80_CHAR_WIDTH*2
+#define _A2_TEXT40_CHAR_HEIGHT _A2_TEXT80_CHAR_HEIGHT
+
+
+enum A2VideoModeBeam_e
+{
+	A2VIDEOBEAM_LEGACY,
+	A2VIDEOBEAM_SHR,
+	A2VIDEOBEAM_TOTAL_COUNT
+};
+
+struct A2BeamVertex {
+	glm::vec2 RelPos;		// Relative position of the vertex
+	glm::vec2 PixelPos;		// Pixel position of the vertex in the Apple 2 screen
+};
+
+class A2WindowBeam
+{
+public:
+	bool IsEnabled() const { return enabled; }
+	void SetEnabled(bool val) { 
+		enabled = val;
+	}
+	bool bNeedsGPUVertexUpdate = false;	// Update the GPU if the vertex data has changed
+
+	A2WindowBeam()
+		: enabled(false)
+		, video_mode(A2VIDEOBEAM_TOTAL_COUNT)
+		, shaderProgram(nullptr)
+	{
+		// Assign the vertex array.
+		// The first 2 values are the relative XY, bound from -1 to 1.
+		// The A2WindowBeam always covers the whole screen, so from -1 to 1 on both axes
+		// The second pair of values is the actual pixel value on screen (560x384, etc...)
+		// Set them to 0, they'll be defined later from the video manager
+		vertices.push_back(A2BeamVertex({glm::vec2(-1,  1), glm::ivec2(0, 0)}));	// top left
+		vertices.push_back(A2BeamVertex({glm::vec2( 1, -1), glm::ivec2(0, 0)}));	// bottom right
+		vertices.push_back(A2BeamVertex({glm::vec2( 1,  1), glm::ivec2(0, 0)}));	// top right
+		vertices.push_back(A2BeamVertex({glm::vec2(-1,  1), glm::ivec2(0, 0)}));	// top left
+		vertices.push_back(A2BeamVertex({glm::vec2(-1, -1), glm::ivec2(0, 0)}));	// bottom left
+		vertices.push_back(A2BeamVertex({glm::vec2( 1, -1), glm::ivec2(0, 0)}));	// bottom right
+	};
+	~A2WindowBeam();
+	void Define(A2VideoModeBeam_e video_mode, Shader* _shaderProgram);
+	void Render();
+
+	Shader* GetShaderProgram() { return shaderProgram; };
+	void SetShaderProgram(Shader* _shader) { shaderProgram = _shader; };
+	A2VideoModeBeam_e Get_video_mode() const { return video_mode; }
+
+private:
+	void Reset();
+
+    bool enabled;           		// if not enabled, doesn't get rendered
+	A2VideoModeBeam_e video_mode;	// Which video mode is used
+	Shader* shaderProgram;			// Shader used
+	uXY screen_count;				// width,height in pixels of visible screen area of window
+
+	unsigned int VRAMTEX = UINT_MAX;		// VRAM buffer texture. Holds R as MAIN, G and AUX, B as flags
+
+	std::vector<A2BeamVertex> vertices;		// Vertices with XYRelative and XYPixels
+	unsigned int VAO = UINT_MAX;			// Vertex Array Object (holds buffers that are vertex related)
+	unsigned int VBO = UINT_MAX;			// Vertex Buffer Object (holds vertices)
+};
+
+#endif // A2WINDOWBEAM_H
