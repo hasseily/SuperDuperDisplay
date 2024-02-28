c844b0bab69713cbe200f49dad53381d2a1d8500
Henri Asseily <henri@asseily.com>
2024-02-28 10:39:30 +0100

    Interlacing. Non-double resolutions working.

--- A2VideoManager.cpp
+++ A2VideoManager.cpp
@@ -59,6 +59,45 @@ static uint32_t gPaletteRGB[] =
 	SETRGBCOLOR(/*WHITE,*/      0xFF,0xFF,0xFF),
 };
 
+// Memory offsets for TEXT/D/LGR and D/HGR modes
+// The rows aren't contiguous in Apple 2 RAM.
+// They're interlaced because WOZ chip optimization.
+
+// Apple 2 TEXT row offset interlacing in RAM
+static uint16_t g_RAM_TEXTOffsets[] =
+{
+	0x0000, 0x0080, 0x0100, 0x0180, 0x0200, 0x0280, 0x0300, 0x0380,
+	0x0028, 0x00A8, 0x0128, 0x01A8, 0x0228, 0x02A8, 0x0328, 0x03A8,
+	0x0050, 0x00D0, 0x0150, 0x01D0, 0x0250, 0x02D0, 0x0350, 0x03D0
+};
+// Apple 2 HGR row offsets interlacing in RAM
+static uint16_t g_RAM_HGROffsets[] = {
+	0x0000, 0x0400, 0x0800, 0x0C00, 0x1000, 0x1400, 0x1800, 0x1C00,
+	0x0080, 0x0480, 0x0880, 0x0C80, 0x1080, 0x1480, 0x1880, 0x1C80,
+	0x0100, 0x0500, 0x0900, 0x0D00, 0x1100, 0x1500, 0x1900, 0x1D00,
+	0x0180, 0x0580, 0x0980, 0x0D80, 0x1180, 0x1580, 0x1980, 0x1D80,
+	0x0200, 0x0600, 0x0A00, 0x0E00, 0x1200, 0x1600, 0x1A00, 0x1E00,
+	0x0280, 0x0680, 0x0A80, 0x0E80, 0x1280, 0x1680, 0x1A80, 0x1E80,
+	0x0300, 0x0700, 0x0B00, 0x0F00, 0x1300, 0x1700, 0x1B00, 0x1F00,
+	0x0380, 0x0780, 0x0B80, 0x0F80, 0x1380, 0x1780, 0x1B80, 0x1F80,
+	0x0028, 0x0428, 0x0828, 0x0C28, 0x1028, 0x1428, 0x1828, 0x1C28,
+	0x00A8, 0x04A8, 0x08A8, 0x0CA8, 0x10A8, 0x14A8, 0x18A8, 0x1CA8,
+	0x0128, 0x0528, 0x0928, 0x0D28, 0x1128, 0x1528, 0x1928, 0x1D28,
+	0x01A8, 0x05A8, 0x09A8, 0x0DA8, 0x11A8, 0x15A8, 0x19A8, 0x1DA8,
+	0x0228, 0x0628, 0x0A28, 0x0E28, 0x1228, 0x1628, 0x1A28, 0x1E28,
+	0x02A8, 0x06A8, 0x0AA8, 0x0EA8, 0x12A8, 0x16A8, 0x1AA8, 0x1EA8,
+	0x0328, 0x0728, 0x0B28, 0x0F28, 0x1328, 0x1728, 0x1B28, 0x1F28,
+	0x03A8, 0x07A8, 0x0BA8, 0x0FA8, 0x13A8, 0x17A8, 0x1BA8, 0x1FA8,
+	0x0050, 0x0450, 0x0850, 0x0C50, 0x1050, 0x1450, 0x1850, 0x1C50,
+	0x00D0, 0x04D0, 0x08D0, 0x0CD0, 0x10D0, 0x14D0, 0x18D0, 0x1CD0,
+	0x0150, 0x0550, 0x0950, 0x0D50, 0x1150, 0x1550, 0x1950, 0x1D50,
+	0x01D0, 0x05D0, 0x09D0, 0x0DD0, 0x11D0, 0x15D0, 0x19D0, 0x1DD0,
+	0x0250, 0x0650, 0x0A50, 0x0E50, 0x1250, 0x1650, 0x1A50, 0x1E50,
+	0x02D0, 0x06D0, 0x0AD0, 0x0ED0, 0x12D0, 0x16D0, 0x1AD0, 0x1ED0,
+	0x0350, 0x0750, 0x0B50, 0x0F50, 0x1350, 0x1750, 0x1B50, 0x1F50,
+	0x03D0, 0x07D0, 0x0BD0, 0x0FD0, 0x13D0, 0x17D0, 0x1BD0, 0x1FD0
+};
+
 // Gotta have a transparency global just in case
 // static uint32_t gRGBTransparent = 0;
 
@@ -398,7 +437,7 @@ void A2VideoManager::BeamIsAtPosition(uint32_t _x, uint32_t y)
 	
 	// Set xx to 0 when after HBLANK. HBLANK is always at the start of the line
 	// However, VBLANK is at the end of the screen so we can use y as is
-	auto xx = _x - CYCLES_HBLANK;
+	uint32_t xx = _x - CYCLES_HBLANK;	// xx is always positive here
 	if (IsSoftSwitch(A2SS_SHR))
 	{
 		bVBlankHasSHR = true;		// at least 1 byte in this vblank cycle is in SHR
@@ -421,7 +460,7 @@ void A2VideoManager::BeamIsAtPosition(uint32_t _x, uint32_t y)
 	
 	// The byte isn't SHR, it's legacy
 	bVBlankHasLegacy = true;	// at least 1 byte in this vblank cycle is not SHR
-	auto byteStartPtr = a2legacy_vram + (40 * 4 * y) + (xx * 4);
+	auto byteStartPtr = a2legacy_vram + ((40 * y) + xx) * 4;	// 4 bytes in VRAM for each byte on screen
 	// the flags byte is:
 	// bits 0-2: mode (TEXT, DTEXT, LGR, DLGR, HGR, DHGR, DHGRMONO)
 	// bit 3: ALT charset for TEXT
@@ -486,14 +525,14 @@ void A2VideoManager::BeamIsAtPosition(uint32_t _x, uint32_t y)
 		uint32_t startMem = _A2VIDEO_TEXT1_START;
 		if ((flags < 3) && isPage2)		// check for page 2 (DLGR doesn't have it)
 			startMem = _A2VIDEO_TEXT2_START;
-		byteStartPtr[0] = *(SDHRManager::GetInstance()->GetApple2MemPtr() + startMem + ((y / 8) * 40) + xx);
-		byteStartPtr[1] = *(SDHRManager::GetInstance()->GetApple2MemAuxPtr() + startMem + ((y / 8) * 40) + xx);
+		byteStartPtr[0] = *(SDHRManager::GetInstance()->GetApple2MemPtr() + startMem + g_RAM_TEXTOffsets[y / 8] + xx);
+		byteStartPtr[1] = *(SDHRManager::GetInstance()->GetApple2MemAuxPtr() + startMem + g_RAM_TEXTOffsets[y / 8] + xx);
 	} else {		// D/HIRES
 		uint32_t startMem = _A2VIDEO_HGR1_START;
 		if (isPage2)
 			startMem = _A2VIDEO_HGR2_START;
-		byteStartPtr[0] = *(SDHRManager::GetInstance()->GetApple2MemPtr() + startMem + (y * 40) + xx);
-		byteStartPtr[1] = *(SDHRManager::GetInstance()->GetApple2MemAuxPtr() + startMem + (y * 40) + xx);
+		byteStartPtr[0] = *(SDHRManager::GetInstance()->GetApple2MemPtr() + startMem + g_RAM_HGROffsets[y] + xx);
+		byteStartPtr[1] = *(SDHRManager::GetInstance()->GetApple2MemAuxPtr() + startMem + g_RAM_HGROffsets[y] + xx);
 	}
 	byteStartPtr[2] = flags;
 	byteStartPtr[3] = colors;
