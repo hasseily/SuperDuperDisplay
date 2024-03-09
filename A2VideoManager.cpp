6775da6bfc4e2db1fbbf03ddfc0a5d06a6f6785c
hasseily <henri@asseily.com>
2024-03-09 10:45:28 +0200

    memset fix

--- A2VideoManager.cpp
+++ A2VideoManager.cpp
@@ -163,8 +163,8 @@ void A2VideoManager::Initialize()
 		vrams_array[i].frame_idx = 0;
 		vrams_array[i].use_legacy = true;
 		vrams_array[i].use_shr = false;
-		memset(vrams_array[i].vram_legacy, _BEAM_VRAM_SIZE_LEGACY, 0);
-		memset(vrams_array[i].vram_shr, _BEAM_VRAM_SIZE_SHR, 0);
+		memset(vrams_array[i].vram_legacy, 0, _BEAM_VRAM_SIZE_LEGACY);
+		memset(vrams_array[i].vram_shr, 0, _BEAM_VRAM_SIZE_SHR);
 	}
 	vrams_write = &vrams_array[current_frame_idx % 2];
 	vrams_read = &vrams_array[1 - (current_frame_idx % 2)];
