4763ca856fedad24b98c60ee1e099291de82b42f
Henri Asseily <henri@asseily.com>
2025-01-11 18:29:25 +0200

    Commented out test Video7 support.

--- MemoryManager.cpp
+++ MemoryManager.cpp
@@ -1,5 +1,6 @@
 #include "MemoryManager.h"
 #include "CycleCounter.h"
+#include "A2VideoManager.h"
 #include <iostream>
 #include <sstream>
 
@@ -59,6 +60,76 @@ void MemoryManager::ProcessSoftSwitch(uint16_t addr, uint8_t val, bool rw, bool 
 {
 	(void)is_iigs;		// mark as unused -- Appletini brings iigs features to the //e
 	//std::cerr << "Processing soft switch " << std::hex << (uint32_t)addr << " RW: " << (uint32_t)rw << " 2gs: " << (uint32_t)is_iigs << std::endl;
+
+	/*
+	* The below is a hack and just here to remember the code.
+	// This is to handle Video-7 cards, but Appletini needs to register as a Video-7 card!
+	// Check the Video-7 special mode changes
+	// using a 2-bit state register
+	// They're activated when the DHIRES (!AN3) switch is toggled exactly 5 times while A2SS_MIXED is OFF:
+	// OFF-ON-OFF-ON-OFF
+	// At each of the 2 ON, it sets a bit based on the 80COL value
+
+	if ((addr & 0b1) == 0xC05E)		// Case of DHIRES (!AN3) being hit: 0xC05E and 0xC05F
+	{
+		if (IsSoftSwitch(A2SS_MIXED))	// MIXED should always be OFF for the Video7 modes
+		{
+			stateAN3Video7 = 0;
+			goto AFTERVIDEO7;
+		}
+		bool setOn = (addr == 0xC05E);
+		if ((stateAN3Video7 == 0) && setOn)	// Not a start of sequence, it needs to start set to off
+			goto AFTERVIDEO7;
+		if ((stateAN3Video7 & 0b1) != setOn)		// toggle
+		{
+			++stateAN3Video7;
+			std::cerr << "toggled AN3 " << stateAN3Video7 << std::endl;
+		}
+		if (stateAN3Video7 == 2)
+		{
+			flagsVideo7 = !IsSoftSwitch(A2SS_80COL);
+		}
+		else if (stateAN3Video7 == 4)
+		{
+			flagsVideo7 += 2 * !IsSoftSwitch(A2SS_80COL);
+		}
+		else if (stateAN3Video7 == 5)	// 5 total toggles, Video-7 is switching modes!
+		{
+			stateAN3Video7 = 0;
+			// Video7 special modes:
+			// 00 : 140x192
+			// 01 : 160x192
+			// 10: MIX mode (ie. COL140Mixed mode) 140x192 + 560x192
+			// 11 : 560x192 monochrome
+			SetSoftSwitch(A2SS_DHGRMONO, false);
+			A2VideoManager::GetInstance()->bUseDHGRCOL140Mixed = false;
+			switch (flagsVideo7)
+			{
+			case 0b00:		// MIXED ON  80COL ON : 140x192
+				// Default
+				break;
+			case 0b01:		// MIXED OFF 80COL ON : 160x192
+				// TODO: NOT HANDLED
+				break;
+			case 0b10:		// MIX mode (ie. COL140Mixed mode) 140x192 + 560x192
+				// TODO: Hack. Don't force usage of A2VideoManager. Instead use another SS variable
+				// for A2VideoManager to determine what to do, or have a special Video7 module
+				A2VideoManager::GetInstance()->bUseDHGRCOL140Mixed = true;
+				break;
+			case 0b11:		// 560x192 monochrome
+				SetSoftSwitch(A2SS_DHGRMONO, true);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	else {
+		stateAN3Video7 = 0;			// Reset the toggle state
+	}
+	*/
+
+AFTERVIDEO7:
 	switch (addr)
 	{
 	case 0xC000:	// 80STOREOFF
@@ -141,10 +212,10 @@ void MemoryManager::ProcessSoftSwitch(uint16_t addr, uint8_t val, bool rw, bool 
 	case 0xC057:	// HIRESON
 		a2SoftSwitches |= A2SS_HIRES;
 		break;
-	case 0xC05E:	// DHIRESON
+	case 0xC05E:	// DHIRESON	(AN3 RESET)
 		a2SoftSwitches |= A2SS_DHGR;
 		break;
-	case 0xC05F:	// DHIRESOFF
+	case 0xC05F:	// DHIRESOFF (AN3 SET)
 		a2SoftSwitches &= ~A2SS_DHGR;
 		break;
 	case 0xC021:	// MONOCOLOR
