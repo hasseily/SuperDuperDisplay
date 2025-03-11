21a110cd4158b83bcd58d58c985c3fa241628b38
Henri Asseily <henri@asseily.com>
2025-03-11 17:35:33 +0200

    bug fix GL_CLAMP_TO_BORDER

--- PostProcessor.cpp
+++ PostProcessor.cpp
@@ -531,8 +531,8 @@ void PostProcessor::Render(SDL_Window* window, GLuint inputTextureSlot, GLuint s
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 		} else {
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
         }
 		glActiveTexture(_TEXUNIT_PP_BEZEL);
 		glBindTexture(GL_TEXTURE_2D, bezelImageAsset.tex_id);
